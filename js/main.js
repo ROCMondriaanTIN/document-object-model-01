733f5abce1926e7c2db70946de162033a83e71d7
Chris0546 <302717645@student.rocmondriaan.nl>
2025-01-13 11:40:11 +0100

    afmaken

--- js/main.js
+++ js/main.js
@@ -3,8 +3,19 @@ const subTitle = "Alles over planeten";
 const planets = ["Mercurius", "Venus", "Aarde", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus"];
 const routes = ["Home", "Universum", "Planeten"];
 
+const h1Element = document.querySelector(".title");
+h1Element.textContent = "Planeten";
+
+const text = document.querySelector("p");
+text.textContent = "Alles over planeten";
 
 
+const textTwo = document.querySelector(".ol");
+textTwo.innerHTML = "";
+for(let i = 0; i < planets.length; i++){
+    const planet = planets[i];
+    textTwo.innerHTML += `<li> ${planet} </li>`;
+}
 
 
 function addPlanet(){
